40a5434cf1db9985abefc9c34efaf915dddb5c0a
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2023-10-27 13:40:12 +0530

    Love babbar sheet

---
+++ rotate an array.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+
+vector<int> rotateArray(vector<int>arr, int k) {
+    vector<int>ans;
+    for(int i=k;i<arr.size();i++){
+        ans.push_back(arr[i]);
+    }
+    for(int i = 0;i<k;i++){
+        ans.push_back(arr[i]);
+    }
+
+    return ans;
+
+      
+}

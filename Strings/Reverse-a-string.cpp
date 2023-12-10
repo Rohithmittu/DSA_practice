0cf2069e16ac8b9dc54bd4fc9c78e1834fdb8bd2
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2023-12-10 15:40:18 +0530

    lb problems

---
+++ Strings/Reverse-a-string.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+string reverseString(string str)
+{
+	int start = 0;
+	int end = str.length() -1;
+
+	while(start<=end){
+		swap(str[start],str[end]);
+		start++;
+		end--;
+	}
+	return str;
+}

18518019dbd1482483c1d7ac6699fc7083aa4cb4
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2024-01-11 13:22:10 +0530

    lb problems

---
+++ Strings/rotated_string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+bool solve(string &s, string &goal){
+    if(s.size()> goal.size()) return false;
+    if(s.size() < goal.size()) return false;
+    s+=s;
+    if(s.find(goal)< s.size()){
+        return true;
+        
+    } else{
+        return false;
+
+    }
+
+}
+
+    bool rotateString(string s, string goal) {
+        return solve(s, goal);
+    }
+};

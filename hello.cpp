91b3a341255fe21bdd5c1d2df3a44df81f0699a1
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2025-09-26 08:27:42 +0530

    setup completed for vscode to ruun c++

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>   // header for input/output
+
+using namespace std;  // so we can use cout without std::
+
+int main() {
+    int x;
+    cin >> x;
+    cout << "Hello, World!  " << x << endl;  // print to console
+    // return 0;  // exit program
+}
+
